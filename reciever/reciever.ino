1a1660006a7230b182e1c8ee2863d501e81ad715
gorgonzolum <gorgonzolum@gmail.com>
2017-07-10 21:17:07 -0500

    add reciever code

---
+++ reciever/reciever.ino
@@ -0,0 +1,38 @@
+/* MFSC PAXC Rocket Telemetry Reciever
+ * License: MIT
+ */
+
+#include <SoftwareSerial.h>
+
+// Tx/Rx pins for HC12
+const byte hc12RxPin = 4;
+const byte hc12TxPin = 5;
+const byte hc12SetPin = 6;
+
+SoftwareSerial hc12(hc12TxPin, hc12RxPin);
+
+void setup() {
+  Serial.begin(9600);
+  hc12.begin(9600);
+  pinMode(hc12SetPin, OUTPUT);
+  digitalWrite(hc12SetPin, LOW);
+  delay(100);
+  hc12.write("AT+DEFAULTS");
+  delay(100);
+  hc12.write("AT+FU4");
+  delay(100);
+  Serial.print("Reading: " + hc12.readString() + "\n"); 
+  delay(50);
+  hc12.write("AT+RX");
+  delay(100);
+  Serial.print("Parameters: " + hc12.readString() + "\n");
+  
+  digitalWrite(hc12SetPin, HIGH);
+  delay(100);
+}
+
+void loop() {
+  if (hc12.available() > 0){
+    Serial.write(hc12.read());
+  }
+}
--- transmitter/rocket_telemetry.ino
+++ transmitter/rocket_telemetry.ino
@@ -1,5 +1,6 @@
 /*  MFSC PAXC Rocket Telemetry Code
- *  
+ *  License: MIT
+ *
  *  Libraries Needed:
  *    Sparkfun MPU9250: https://github.com/sparkfun/MPU-9250_Breakout
  *    Adafruit BMP085: https://github.com/adafruit/Adafruit-BMP085-Library
